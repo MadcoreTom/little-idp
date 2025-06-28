fd1d01ddba22795e9f0705c47e6fa50256e69007
MadcoreTom <none>
2025-06-28 22:01:37 +1000

    basic html form post

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-// This is a comment, and is ignored by the compiler.
-// You can test this code by clicking the "Run" button over there ->
-// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
-// shortcut.
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::http::header::ContentType;
+use bytes::Bytes;
+use url::form_urlencoded;
+use std::collections::HashMap;
 
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
+static LOGIN_HTML : &str = include_str!("login.html");
 
-// This is the main function.
-fn main() {
-    // Statements here are executed when the compiled binary is called.
+async fn login_form() -> impl Responder {
+    HttpResponse::Ok()
+    .content_type(ContentType::html())
+    .body(LOGIN_HTML)
+}
 
-    // Print text to the console.
-    println!("Hello World!");
+async fn login_submit(body: Bytes) -> impl Responder {
+    // Parse formdata body into map 
+    let mut form_data: HashMap<String, String> = HashMap::new();
+    for (key, value) in form_urlencoded::parse(body.as_ref()) {
+        form_data.insert(key.into_owned(), value.into_owned());
+    }
+
+    // So in here we verify the username and password
+    // Then make a time-limited token
+    // and include it in a redirect
+
+    HttpResponse::Ok()
+    .content_type(ContentType::html())
+    .body(form_data["user"].clone())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(
+            web::scope("/oauth")
+            .route("/authorize", web::get().to(login_form))
+            .route("/authorize", web::post().to(login_submit))
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
